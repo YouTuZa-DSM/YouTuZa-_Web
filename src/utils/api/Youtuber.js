25fa915c64de39e65470d859a315383d91839b7d
inung1004 <dksdbswl10430@naver.com>
2024-07-17 19:55:48 +0900

    fix :: axios -> instance

--- src/utils/api/Youtuber.js
+++ src/utils/api/Youtuber.js
@@ -1,13 +1,13 @@
-import axios from "axios";
 import toast from "react-hot-toast";
 import { useQuery } from "react-query";
+import instance from "../axios";
 
 const BASE_URL = `${process.env.REACT_APP_PUBLIC_BASE_URL}/youtuber`;
 
 export const useGetYoutuberRanking = () => {
   return useQuery(
     ["youtuber_ranking"],
-    async () => axios.get(`${BASE_URL}/top`),
+    async () => instance.get(`${BASE_URL}/top`),
     {
       onSuccess: (e) => {
         console.log(e);
@@ -23,7 +23,7 @@ export const useGetYoutuberRanking = () => {
 export const useGetIncreaseYoutuber = () => {
   return useQuery(
     ["youtuber_increase"],
-    async () => axios.get(`${BASE_URL}/increase`),
+    async () => instance.get(`${BASE_URL}/increase`),
     {
       onSuccess: (e) => {
         return e;
@@ -36,7 +36,7 @@ export const useGetIncreaseYoutuber = () => {
 };
 
 export const useGetYoutuber = () => {
-  return useQuery(["youtuber"], async () => axios.get(`${BASE_URL}/all`), {
+  return useQuery(["youtuber"], async () => instance.get(`${BASE_URL}/all`), {
     onSuccess: (e) => {
       return e;
     },
@@ -49,7 +49,8 @@ export const useGetYoutuber = () => {
 export const useGetYoutuberDetail = (youtuberName) => {
   return useQuery(
     ["youtuber_detail", youtuberName],
-    async () => axios.get(`${BASE_URL}/details?youtuber-name=${youtuberName}`),
+    async () =>
+      instance.get(`${BASE_URL}/details?youtuber-name=${youtuberName}`),
     {
       onSuccess: (e) => {
         return e;
@@ -63,7 +64,7 @@ export const useGetYoutuberDetail = (youtuberName) => {
 
 export const useGetYoutuberChart = (youtuberName) => {
   return useQuery(
-    async () => axios.get(`${BASE_URL}/chart?youtuber-name=${youtuberName}`),
+    async () => instance.get(`${BASE_URL}/chart?youtuber-name=${youtuberName}`),
     {
       onSuccess: (e) => {
         return e;
@@ -79,7 +80,7 @@ export const useGetYoutuberAskPrice = (youtuberName) => {
   return useQuery(
     ["asking_price", youtuberName],
     async () =>
-      axios.get(`${BASE_URL}/ask-price?youtuber-name=${youtuberName}`),
+      instance.get(`${BASE_URL}/ask-price?youtuber-name=${youtuberName}`),
     {
       onSuccess: (e) => {
         return e;
