d1726976a86734a6bbb83b7def6b1b39cada16be
EnsDevelop <chemins60217@gmail.com>
2024-07-17 11:00:13 +0900

    fix :: token name

--- src/utils/functions/TokenManager.js
+++ src/utils/functions/TokenManager.js
@@ -1,14 +1,14 @@
-export const setToken = (accessToken: string, refreshToken: string) => {
-  localStorage.setItem("accessToken", accessToken);
-  localStorage.setItem("refreshToken", refreshToken);
+export const setToken = (accessToken, refreshToken) => {
+  localStorage.setItem("access_token", accessToken);
+  localStorage.setItem("refresh_token", refreshToken);
 };
 
 export const removeToken = () => {
-  localStorage.removeItem("accessToken");
+  localStorage.removeItem("access_token");
 };
 
 export const getToken = () => {
-  const accessToken = localStorage.getItem("accessToken");
+  const accessToken = localStorage.getItem("access_token");
 
   return accessToken;
 };
